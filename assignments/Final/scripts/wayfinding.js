3e38c630c46c83d654370dd97fc3aab0cbadcc80
BaileeR <bailee41@gmail.com>
2018-04-07 20:03:32 -0600

    Final Submission

--- assignments/Final/scripts/wayfinding.js
+++ assignments/Final/scripts/wayfinding.js
@@ -1,16 +1,15 @@
-var urlString = document.location.href;
-var urlArray = urlString.split('/');
-var pageHREF = urlArray[urlArray.length - 1];
+var urlString, urlArray, pageHREF, menu, i, currentURL;
+urlString = document.location.href;
+urlArray = urlString.split('/');
+pageHREF = urlArray[urlArray.length - 1];
 
 if (pageHREF !== "") {
-    var menu = document.querySelectorAll('ul#mainmenu li a');
-    var i;
+    menu = document.querySelectorAll('ul#mainmenu li a');
     for (i = 0; i < menu.length; i++) {
-        var currentURL = (menu[i].getAttribute("href"));
-        menu[i].parentNode.className = "";
+        currentURL = (menu[i].getAttribute('href'));
+        menu[i].parentNode.className = '';
         if (currentURL === pageHREF) {
-            menu[i].parentNode.className = "active";
-    console.log("is this workign");
+            menu[i].parentNode.className = 'active';
         }
     }
 }
